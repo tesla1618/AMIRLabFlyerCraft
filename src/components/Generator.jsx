37fd0b7c11dbdceba0c61f550a7b7cf227b0e5d2
Rajieb <67636538+tesla1618@users.noreply.github.com>
2025-02-02 23:21:30 +0600

    Added comma separated functionality in Author Names

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -16,23 +16,38 @@ const Generator = () => {
   const [title, setTitle] = useState("");
   const [jName, setjName] = useState("");
   const [authorNames, setauthorNames] = useState([]);
-  const dummyauthorNames = ["Nikola Tesla", "Albert Einstein", "Marie Curie", "Isaac Newton", "Richard Feynman", "J. Robert Oppenheimer"];
+  const dummyauthorNames = [
+    "Nikola Tesla",
+    "Albert Einstein",
+    "Marie Curie",
+    "Isaac Newton",
+    "Richard Feynman",
+    "J. Robert Oppenheimer",
+  ];
   const [currentAuthorName, setcurrentAuthorName] = useState("");
   const [noAuthor, setnoAuthor] = useState(false);
 
   function addAuthor() {
-    if (currentAuthorName.trim() !== "") {
-      setauthorNames((prevAuthors) => [...prevAuthors, currentAuthorName]);
-      setcurrentAuthorName("");
-      setnoAuthor(false);
-    } else {
-      setnoAuthor(true);
-      console.log("No Author");
-    }
+    const authors = currentAuthorName.split(",");
+    const filteredAuthors = authors.filter((author) => author.trim() !== "");
+    setauthorNames((prevAuthors) => [...prevAuthors, ...filteredAuthors]);
+    setcurrentAuthorName("");
+    setnoAuthor(false);
+
+    // if (currentAuthorName.trim() !== "") {
+    //   setauthorNames((prevAuthors) => [...prevAuthors, currentAuthorName]);
+    //   setcurrentAuthorName("");
+    //   setnoAuthor(false);
+    // } else {
+    //   setnoAuthor(true);
+    //   console.log("No Author");
+    // }
   }
 
   const deleteAuthor = (index) => {
-    setauthorNames((previousAuthors) => previousAuthors.filter((_, i) => i !== index));
+    setauthorNames((previousAuthors) =>
+      previousAuthors.filter((_, i) => i !== index)
+    );
   };
 
   function ChangeJournal(e) {
@@ -59,7 +74,9 @@ const Generator = () => {
     try {
       socialTemplate.current.style.width = "609px";
       socialTemplate.current.style.height = "fit-content";
-      const dataUrl = await htmlToImage.toPng(socialTemplate.current, { quality: 1.9 });
+      const dataUrl = await htmlToImage.toPng(socialTemplate.current, {
+        quality: 1.9,
+      });
 
       socialTemplate.current.style.width = "";
       socialTemplate.current.style.height = "";
@@ -69,7 +86,10 @@ const Generator = () => {
       link.click();
     } catch (error) {
       if (error instanceof Event) {
-        console.error("Error during image generation. Ignoring event object:", error);
+        console.error(
+          "Error during image generation. Ignoring event object:",
+          error
+        );
       } else {
         console.error("Error during image generation:", error);
       }
@@ -85,13 +105,41 @@ const Generator = () => {
           <div className="bg-base-100 shadow-md rounded-md p-10">
             <h3 className="text-xl border-b pb-4 ">Information</h3>
             <div className="py-5 flex flex-col gap-y-2">
-              <input onChange={ChangeJournal} type="text" placeholder="Journal/Conference/Book Chapter Name" className="input input-bordered focus:input-primary" />
-              <input onChange={ChangeTitle} type="text" placeholder="Paper Title" className="input input-bordered focus:input-primary" />
-              <input onChange={changeDP} type="file" className="file-input input-bordered focus:input-primary" accept="image/*" />
+              <input
+                onChange={ChangeJournal}
+                type="text"
+                placeholder="Journal/Conference/Book Chapter Name"
+                className="input input-bordered focus:input-primary"
+              />
+              <input
+                onChange={ChangeTitle}
+                type="text"
+                placeholder="Paper Title"
+                className="input input-bordered focus:input-primary"
+              />
+              <input
+                onChange={changeDP}
+                type="file"
+                className="file-input input-bordered focus:input-primary"
+                accept="image/*"
+              />
               <div className="">
                 <div className="flex flex-row gap-x-2">
-                  <input placeholder="Author Name" type="text" className={!noAuthor ? "input flex-1 w-3/5 input-bordered focus:input-primary " : "input flex-1 w-1/5 input-error focus:input-primary"} value={currentAuthorName} onChange={(e) => setcurrentAuthorName(e.target.value)} />
-                  <button className="flex-2 w-2/5 btn bg-green-600  hover:bg-black text-white" onClick={addAuthor}>
+                  <input
+                    placeholder="Author Name"
+                    type="text"
+                    className={
+                      !noAuthor
+                        ? "input flex-1 w-3/5 input-bordered focus:input-primary "
+                        : "input flex-1 w-1/5 input-error focus:input-primary"
+                    }
+                    value={currentAuthorName}
+                    onChange={(e) => setcurrentAuthorName(e.target.value)}
+                  />
+                  <button
+                    className="flex-2 w-2/5 btn bg-green-600  hover:bg-black text-white"
+                    onClick={addAuthor}
+                  >
                     Add
                   </button>
                 </div>
@@ -107,8 +155,13 @@ const Generator = () => {
                   <ul className="mt-3">
                     {authorNames.map((text, index) => (
                       <li className="mb-2 flex flex-row gap-x-2" key={index}>
-                        <p className="shadow-md border rounded-lg px-4 py-3 flex-1 w-3/4">{text}</p>
-                        <button className="btn bg-red-600 text-white hover:bg-black text-white" onClick={() => deleteAuthor(index)}>
+                        <p className="shadow-md border rounded-lg px-4 py-3 flex-1 w-3/4">
+                          {text}
+                        </p>
+                        <button
+                          className="btn bg-red-600 text-white hover:bg-black text-white"
+                          onClick={() => deleteAuthor(index)}
+                        >
                           Delete
                         </button>
                       </li>
@@ -117,52 +170,93 @@ const Generator = () => {
                 </div>
               </div>
               <div className="flex flex-row gap-x-3">
-                <input type="checkbox" checked={authorIndex} onChange={() => setauthorIndex(!authorIndex)} className="toggle toggle-primary" /> Show Author Index
+                <input
+                  type="checkbox"
+                  checked={authorIndex}
+                  onChange={() => setauthorIndex(!authorIndex)}
+                  className="toggle toggle-primary"
+                />{" "}
+                Show Author Index
               </div>
             </div>
             {/* <div className="col-md-6 col-12 text-center">{authorDp && <img id="image-preview" alt="Preview" className="object-fit-cover rounded-circle" style={{ width: "200px", height: "200px", marginTop: "10px" }} />}</div> */}
           </div>
           <div>
-            <div ref={socialTemplate} className="shadow-md p-10 generated-image-wrap">
+            <div
+              ref={socialTemplate}
+              className="shadow-md p-10 generated-image-wrap"
+            >
               <div className="wave"></div>
               <div className="generated-content">
                 <div className="amirlab-title">
                   <img src={logoCircle} width={80} alt="" />
-                  <p className="text-white font-bold text-2xl ml-3">Advanced Machine Intelligence Research Lab - AMIR Lab</p>
+                  <p className="text-white font-bold text-2xl ml-3">
+                    Advanced Machine Intelligence Research Lab - AMIR Lab
+                  </p>
                 </div>
                 <div className="success-in mt-3">
                   <div className="success-text border-b pb-2">Success In</div>
-                  <div className="journal-name mt-2">{jName ? jName : "Journal Name"}</div>
+                  <div className="journal-name mt-2">
+                    {jName ? jName : "Journal Name"}
+                  </div>
                 </div>
                 <div className="flex flex-row items-center justify-center content-center gap-x-5 mt-6">
                   <div className="flex-1 author-dp w-2/5">
                     <div className="avatar">
-                      <div className="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">{authorDp ? <img width={120} id="image-preview" /> : <img width={120} src={defaultAvatar} />}</div>
+                      <div className="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        {authorDp ? (
+                          <img width={120} id="image-preview" />
+                        ) : (
+                          <img width={120} src={defaultAvatar} />
+                        )}
+                      </div>
                     </div>
                   </div>
                   <div className="flex-2 w-3/5">
                     <div className="paper-title">
                       <div className="icon">
-                        <FeatherIcon icon="file-text" size={19} className="text-white" />
+                        <FeatherIcon
+                          icon="file-text"
+                          size={19}
+                          className="text-white"
+                        />
                       </div>
-                      <p className="text-white">{title ? title : "The title of the paper is prominently featured in this designated section for easy identification and reference"}</p>
+                      <p className="text-white">
+                        {title
+                          ? title
+                          : "The title of the paper is prominently featured in this designated section for easy identification and reference"}
+                      </p>
                     </div>
                     <div className="paper-authors mt-4">
                       <div className="icon">
-                        <FeatherIcon className="text-white" size={19} icon="users" />
+                        <FeatherIcon
+                          className="text-white"
+                          size={19}
+                          icon="users"
+                        />
                       </div>
                       <p className="gap">
                         {authorNames && authorNames.length > 0
                           ? authorNames.map((author, index) => (
-                              <div className="badge badge-ghost mr-1" key={index}>
+                              <div
+                                className="badge badge-ghost mr-1"
+                                key={index}
+                              >
                                 {author}
-                                {authorIndex && <sup className="ml-1">{index + 1}</sup>}
+                                {authorIndex && (
+                                  <sup className="ml-1">{index + 1}</sup>
+                                )}
                               </div>
                             ))
                           : dummyauthorNames.map((dauthor, index) => (
-                              <div className="badge badge-ghost mr-1" key={index}>
+                              <div
+                                className="badge badge-ghost mr-1"
+                                key={index}
+                              >
                                 {dauthor}
-                                {authorIndex && <sup className="ml-1">{index + 1}</sup>}
+                                {authorIndex && (
+                                  <sup className="ml-1">{index + 1}</sup>
+                                )}
                               </div>
                             ))}
                       </p>
